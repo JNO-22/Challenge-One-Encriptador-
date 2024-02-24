3f9da227b15bcf288429e045ed15e4a2d8aa8bc3
Julian <julian.oliva225@gmail.com>
2024-02-23 22:29:48 -0300

    --agregada la propiedad de arrastre a las ventanas

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
-const btns = document.getElementsByClassName("botones");
+// Obtener todos los botones
+const btns = document.getElementsByClassName("botones"); 
 for (let i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", getText);
 }
@@ -8,9 +9,9 @@ function getText() {
   var text = document.getElementById("mensaje").value;
   var output = document.getElementById("window-output-text");
   if (pattern.test(text) || this.id == "boton-copiar") {
+    output.innerText = "";
     switch (this.id) {
       case "btn-encriptar":
-        output.innerText = "";
         encriptarTexto(text);
         break;
       case "btn-desencriptar":
@@ -63,9 +64,9 @@ function mostrarMSJ(textoResultante) {
   const output = document.getElementById("window-output-text");
   escribirMSJ(output, textoResultante, 0, 50);
   document.getElementById("boton-copiar").style.display = "block";
-  for (const box of document.getElementsByClassName("window-output-warning")) {
-    box.style.display = "none";
-  }
+  document.querySelectorAll(".window-output-warning").forEach((el) => {
+    el.style.display = "none";
+  })  
   document.getElementById("mensaje").value = "";
 }
 
@@ -81,7 +82,7 @@ function escribirMSJ(target, mensaje, index, intervalo) {
 
 function copiar() {
   navigator.clipboard
-    .writeText(document.getElementById("window-output-text").innerText)
+    .writeText(document.querySelector("window-output-text").innerText)
     .then(() => {
       document.getElementById("warning-titulo").innerText = "Copiado";
       document.getElementById("warning-texto").innerText = "Ingrese otro texto para continuar";
@@ -98,48 +99,55 @@ function ocultarMSJ() {
 }
 
 
-var object = document.querySelector("#myWindow"),initX,initY,firstX,firstY;
 
-// addevenlistener de todos los elementos de la clase
-  object.addEventListener("mousedown",iniciarMovimiento,false);
+//inicio las funciones para cada ventana
+makeDraggable(document.querySelector('#myWindow'));
+makeDraggable(document.querySelector('#myWindow2'));
+makeDraggable(document.querySelector('#myWindow3'));
 
-// iniciarMovimiento para todos los elementos en un escritorio
-function iniciarMovimiento(e){
-console.log(e);
- e.preventDefault();
- initX = this.offsetLeft;
- initY = this.offsetTop;
- firstX = e.pageX;
- firstY = e.pageY;
 
- this.addEventListener("mousemove",continuarMovimiento,false );
 
- window.addEventListener("mouseup",finMovimiento , false);
+// Esta funcion se encarga de hacer que el elemento se pueda arrastrar
+function makeDraggable(element) {
+  let currentPosX = 0, currentPosY = 0, previousPosX = 0, previousPosY = 0; // Variables globales
+  element.onmousedown = e => {
+    document.querySelectorAll('.window').forEach((el) => { // Ocultar todas las ventanas
+      el.style.zIndex = 1;
+    });
+    element.style.zIndex = 2; // Activar la ventana activa
+  };
+
+  if (element.querySelector('.window-top')) {
+    element.querySelector('.window-top').onmousedown = dragMouseDown; //iniciar el arrastre
+  }
+
+  function dragMouseDown(e) {
+    
+    previousPosX = e.clientX;
+    previousPosY = e.clientY;
+    document.onmouseup = closeDragElement;
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+
+    currentPosX = previousPosX - e.clientX;
+    currentPosY = previousPosY - e.clientY;
+    previousPosX = e.clientX;
+    previousPosY = e.clientY;
+    element.style.top = (element.offsetTop - currentPosY) + 'px';
+    element.style.left = (element.offsetLeft - currentPosX) + 'px';
+  }
+
+  function closeDragElement() {
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
 }
 
-//termina el movimiento eliminando el evento
-function finMovimiento () {
-  this.style.zIndex = "1";
-  object.removeEventListener("mousemove",continuarMovimiento,false );
-}
 
-//continua el movimiento determinando la nueva posicion
-function continuarMovimiento(e) {
-  // Calcula los bordes de la ventana
-  const maxX = window.innerWidth - this.offsetWidth;
-  const maxY = window.innerHeight - this.offsetHeight;
-
-  //Calcula la nueva posicion del elemento respecto al mouse
-  let newX = initX + e.pageX - firstX;
-  let newY = initY + e.pageY - firstY;
-
-  // Limita la nueva posicion dentro de los bordes
-  newX = Math.min(Math.max(0, newX), maxX);
-  newY = Math.min(Math.max(0, newY), maxY);
-
-  // Actualiza la nueva posicion
-  this.style.left = newX + "px";
-  this.style.top = newY + "px";
-}
-
-
+document.addEventListener('click', e => {
+  if (e.target.closest('.round.red')) {
+    e.target.closest('.window').remove();
+  }
+});
